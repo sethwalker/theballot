064b4e18bc9d0579a5b1d5be9a3ca76a212828d6
seth <seth@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2006-08-05 03:11:21 +0000

    more fix confirm link #38
    git-svn-id: https://svn.radicaldesigns.org/voterguides/trunk@47 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- app/models/user_notifier.rb
+++ app/models/user_notifier.rb
@@ -2,19 +2,19 @@ class UserNotifier < ActionMailer::Base
   def signup_notification(user)
     setup_email(user)
     @subject    += 'Please activate your new account'
-    @body[:url]  = "http://{@request.env['HTTP_HOST']}/account/activate/#{user.activation_code}"
+    @body[:url]  = "http://#{APPLICATION_HOST_NAME}/account/activate/#{user.activation_code}"
   end
   
   def activation(user)
     setup_email(user)
     @subject    += 'Your account has been activated!'
-    @body[:url]  = "http://{@request.env['HTTP_HOST']}/"
+    @body[:url]  = "http://#{APPLICATION_HOST_NAME}/"
   end
   
   protected
   def setup_email(user)
     @recipients  = "#{user.email}"
-    @from        = "voterguides@{@request.env['SERVER_NAME']}"
+    @from        = "voterguides@indyvoter.org"
     @subject     = "[voterguides] "
     @sent_on     = Time.now
     @body[:user] = user
