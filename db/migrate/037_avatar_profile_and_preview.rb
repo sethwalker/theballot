6fc84dbc5ab29bf191845381c6744793646f387d
seth <seth@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2006-10-04 23:56:33 +0000

    set up profile and preview avatar thumbnails
    git-svn-id: https://svn.radicaldesigns.org/voterguides/trunk@272 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

---
+++ db/migrate/037_avatar_profile_and_preview.rb
@@ -0,0 +1,17 @@
+class AvatarProfileAndPreview < ActiveRecord::Migration
+  def self.up
+    Avatar.find(:all).each do |a|
+      a.create_or_update_thumbnail(:profile, '300>x300>')
+      a.create_or_update_thumbnail(:preview, '150>x150>')
+    end
+  end
+
+  def self.down
+    Avatar.find_all_by_thumbnail('profile').each do |a|
+      a.destroy
+    end
+    Avatar.find_all_by_thumbnail('preview').each do |a|
+      a.destroy
+    end
+  end
+end
