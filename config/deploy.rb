0b8725f59fb69944e4267a5be391616aab0fad16
rabble <rabble@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2006-11-06 09:38:15 +0000

    updated deploy script
    git-svn-id: https://svn.radicaldesigns.org/voterguides/trunk@676 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,4 @@
-require 'mongrel_cluster/recipes'
-
-require 'config/cap_ssh_ports'
+require 'cap_ssh_ports'
 
 # This defines a deployment "recipe" that you can feed to capistrano
 # (http://manuals.rubyonrails.com/read/book/17). It allows you to automate
@@ -14,7 +12,7 @@ require 'config/cap_ssh_ports'
 # correspond to. The deploy_to path must be the path on each machine that will
 # form the root of the application path.
 
-set :keep_releases, 3 
+set :keep_releases, 3
 set :application, 'theballot'
 set :repository,   'https://svn.radicaldesigns.org/voterguides/trunk/'
 set :svn_username, ''
@@ -32,7 +30,7 @@ set :svn_password, ''
 
 role :web, "theballot@65.74.169.199:8050"
 role :app, "theballot@65.74.169.199:8050"
-role :app,  "theballot@65.74.169.199:8051", :no_release => true
+role :app,  "theballot@65.74.169.199:8051", :no_release => true, :no_symlink => true
 role :db,  "theballot@65.74.169.199:8050", :primary => true
 
 # =============================================================================
@@ -40,8 +38,6 @@ role :db,  "theballot@65.74.169.199:8050", :primary => true
 # =============================================================================
 
 set :deploy_to, "/data/#{application}"
-set :mongrel_conf, "#{deploy_to}/current/config/mongrel_cluster.yml"
-#set :use_sudo, false
 
 # =============================================================================
 # SSH OPTIONS
@@ -50,6 +46,48 @@ task :uname, :roles => [:app, :prod_slave] do
   run "uname -a"
 end
 
+
+  desc <<-DESC
+  Restart the Mongrel processes on the app server by calling restart_mongrel_cluster.
+  DESC
+  task :restart, :roles => :app do
+    restart_mongrel_cluster
+  end
+
+  desc <<-DESC
+  Start the Mongrel processes on the app server by calling start_mongrel_cluster.
+  DESC
+  task :spinner, :roles => :app do
+    start_mongrel_cluster
+  end
+
+
+desc <<-DESC
+Start Mongrel processes on the app server.  This uses the :use_sudo variable to determine whether to use sudo or not. By default, :use_sudo is
+set to true.
+DESC
+task :start_mongrel_cluster , :roles => :app do
+  sudo "/etc/init.d/mongrel_cluster start"
+end
+
+desc <<-DESC
+Restart the Mongrel processes on the app server by starting and stopping the cluster. This uses the :use_sudo
+variable to determine whether to use sudo or not. By default, :use_sudo is set to true.
+DESC
+task :restart_mongrel_cluster , :roles => :app do
+  sudo "/etc/init.d/mongrel_cluster restart"
+end
+
+desc <<-DESC
+Stop the Mongrel processes on the app server.  This uses the :use_sudo
+variable to determine whether to use sudo or not. By default, :use_sudo is
+set to true.
+DESC
+task :stop_mongrel_cluster , :roles => :app do
+  sudo "/etc/init.d/mongrel_cluster stop"
+end
+
+
 # =============================================================================
 # TASKS
 # =============================================================================
@@ -57,12 +95,16 @@ end
 # a role (or set of roles) that each task should be executed on. You can also
 # narrow the set of servers to a subset of a role by specifying options, which
 # must match the options given for the servers to select (like :primary => true)
-
-task :after_update_code, :roles => :app do
+#ln -s #{shared_path}/database.yml #{release_path}/config/database.yml &&
+task :after_update_code, :roles => :app, :except => {:no_symlink => true} do
   run <<-CMD
     cd #{release_path} &&
-    ln -s #{shared_path}/database.yml #{release_path}/config/database.yml &&
-    ln -s #{shared_path}/config/mongrel_cluster.yml #{release_path}/config/mongrel_cluster.yml &&
-    rake tmp:create
+    ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml &&
+    ln -nfs #{shared_path}/config/mongrel_cluster.yml #{release_path}/config/mongrel_cluster.yml
   CMD
 end
+
+task :after_symlink, :roles => :app , :except => {:no_symlink => true} do
+  sudo "ln -nfs #{shared_path}/public/themes #{release_path}/public/themes"
+  sudo "chmod 755 #{release_path}/public/themes/default.liquid"
+end 
