e8dbd4df38c60cc15fceb008ea16ca1359e310ac
seth <seth@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2006-11-06 07:12:23 +0000

    up to 3 mb attached pdfs
    git-svn-id: https://svn.radicaldesigns.org/voterguides/trunk@670 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- app/models/attached_pdf.rb
+++ app/models/attached_pdf.rb
@@ -1,5 +1,5 @@
 class AttachedPdf < Attachment
-  acts_as_attachment :content_type => 'application/pdf', :max_size => 2.megabytes
+  acts_as_attachment :content_type => 'application/pdf', :max_size => 3.megabytes
   validates_as_attachment
   belongs_to :guide
   belongs_to :user
