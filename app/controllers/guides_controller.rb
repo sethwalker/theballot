b2c0f63f3b13c5a92b82402ac8c72471385d4434
seth <seth@75c0e4a9-b6e4-0310-a6ec-d70c6baf2916>
2006-11-30 20:24:43 +0000

    added a credits action and route
    git-svn-id: https://svn.radicaldesigns.org/voterguides/trunk@710 75c0e4a9-b6e4-0310-a6ec-d70c6baf2916

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,12 +1,15 @@
 class GuidesController < ApplicationController
   observer :guide_observer
   prepend_before_filter :find_guide_by_permalink
-  before_filter :login_required, :except => [ :show, :list, :index, :xml, :archive, :by_date, :by_state, :search, :help, :instructions, :tell, :send_message ]
+  before_filter :login_required, :except => [ :show, :list, :index, :xml, :archive, :by_date, :by_state, :search, :help, :instructions, :tell, :send_message, :credits ]
 
   #don't allow guides to be edited after the date of the election
   before_filter :check_date, :only => [ :edit, :update_basics ]
   meantime_filter :scope_published, :except => [ :new, :show, :edit, :update, :destroy, :update_basics, :update_theme, :update_assets, :update_legal, :endorsed_status, :approved_status, :published_status, :order, :admin ]
   meantime_filter :scope_approved_guides, :except => [ :new, :show, :edit, :update, :destroy, :update_basics, :update_theme, :update_assets, :update_legal, :endorsed_status, :approved_status, :published_status, :order ]
+
+  def credits
+  end
 
   def scope_approved_guides
     conditions = "approved_at IS NOT NULL OR legal IS NULL OR legal != '#{Guide::NONPARTISAN}'"
