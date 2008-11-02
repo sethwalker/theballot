a3df22944b4df910e4a0a75403e6f803511fc4a2
theballot <theballot@ey00-s00050.engineyard.com>
2008-11-02 09:21:39 -0800

    validate guides

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :guide
   belongs_to :user
+  validates_presence_of :guide_id, :body
   named_scope :recent, :order => 'comments.created_at DESC', :include => :guide, :conditions => "guides.id"
 end
