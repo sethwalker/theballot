9786b837553a9dd9231748e236baf24734d9f13f
Seth Walker <seth@radicaldesigns.org>
2008-11-02 20:47:52 -0800

    add scope for comments of published guides

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,5 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :guide_id, :body
   named_scope :recent, :order => 'comments.created_at DESC', :include => :guide, :conditions => "guides.id"
+  named_scope :published, :include => :guide, :conditions => ["guides.status = ?", Guide::PUBLISHED]
 end
