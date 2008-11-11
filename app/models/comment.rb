03a088820f08529f43a36494f30c6d671ec28830
Sam Dorman <sam@theleague.com>
2008-11-11 15:20:55 -0800

    add approved scope

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,5 @@ class Comment < ActiveRecord::Base
   validates_presence_of :guide_id, :body
   named_scope :recent, :order => 'comments.created_at DESC', :include => :guide, :conditions => "guides.id"
   named_scope :published, :include => :guide, :conditions => ["guides.status = ?", Guide::PUBLISHED]
+  named_scope :approved, :include => :guide, :conditions => ["guides.approved_at IS NOT NULL OR legal IS NULL OR legal != ?", Guide::NONPARTISAN]
 end
